7f988338b8e1533bb99dbf065b8d7cb6a77b5a6e
iainmcampbell <hey@iainmc.ca>
2015-10-08 14:32:24 -0400

    tweaks

--- src/modules/small/small.directive.js
+++ src/modules/small/small.directive.js
@@ -23,7 +23,7 @@
 
       scope: {},
 
-      // see Readme for explanation of controllerAs
+      // see wiki for explanation of controllerAs
       controller: smallDirectiveController,
       controllerAs: 'vm',
       bindToController: true
