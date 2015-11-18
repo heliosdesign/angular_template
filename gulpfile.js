2864d3b9e7469114b2e824353b045e6dc48ddf2f
iainmcampbell <hey@iainmc.ca>
2015-11-18 16:36:38 -0500

    reload config.json when it changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,20 @@ var del             = require('del');
 var pathExists      = require('path-exists');
 var plugins         = gulpLoadPlugins();
 var runSequence     = require('run-sequence');
+<<<<<<< HEAD
 var inquirer        = require('inquirer');
 var config          = require('./config.json');
+=======
+var fs              = require('fs')
+
+
+/*
+
+  Vars
+
+*/
+var config, jsSources;
+>>>>>>> reload config.json when it changes
 
 var src = {
   base:    './src',
@@ -19,10 +31,13 @@ var dist = {
   min:     './dist/min',
 }
 
-var filesnames = {
+var filenames = {
   css: 'application.min.css',
   js: 'application.min.js'
 }
+
+loadConfigFile();
+
 
 /**
  * Functions and Utilities
@@ -32,38 +47,32 @@ function swallowError(error) {
   this.emit('end');
 };
 
-function fileContents(filePath, file) {
-  return file.contents.toString();
-};
-
-// pass in an array of arrays, get a single concatenated array back
-function concatArrays(arrays){
-  var concatenated = [];
-  arrays.forEach(function(a){
-    concatenated = concatenated.concat(a);
-  })
-  return concatenated;
-}
-
 var svgIcons = gulp.src([
   src.modules + '/**/icon-*.svg',
   '!' + src.modules + '/fonts/**.*'
 ])
-.pipe(plugins.svgmin())
-.pipe(plugins.svgstore({ inlineSvg: true }));
+  .pipe(plugins.svgmin())
+  .pipe(plugins.svgstore({ inlineSvg: true }));
 
-// libs from bower + modules
-var jsSources = config.js.lib.concat([
-  src.modules + '/core/app.js',
-  src.modules + '/**/init.js',
-  src.modules + '/**/*.js'
-]);
+// load the config file using fs (require caches it so we can’t reload it)
+function loadConfigFile(){
+  config = JSON.parse( fs.readFileSync('./config.json', 'utf8') );
+
+  // add all js files in src/modules/ to libs list from config.json (bower, etc)
+  // the app, init, * order is necessary so angular recognizes modules properly
+  jsSources = config.js.lib.concat([
+    src.modules + '/core/app.js',
+    src.modules + '/**/init.js',
+    src.modules + '/**/*.js'
+  ]);
+};
+
 
 /**
  * Tasks
  */
 
-// Cleaning
+// Cleaning (except for dist/.git)
 gulp.task('clean', function(){
   return del([ dist.base + '/*', '!'+dist.base+'/.git*' ]);
 });
@@ -74,8 +83,8 @@ gulp.task('clean', function(){
 
 */
 
-// SASS compiling task: all sass files to a single CSS file,
-// except base.sass which gets inlined
+// SASS compiling: all sass files -> single CSS file
+// (except base.sass which gets inlined)
 gulp.task('sass', function() {
   return gulp.src([ src.modules + '/**/*.sass', '!'+src.modules+'/core/sass/base.sass' ])
     .pipe(plugins.sourcemaps.init())
@@ -86,7 +95,7 @@ gulp.task('sass', function() {
       .on('error', swallowError)
       .pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
       .pipe(plugins.cssmin())
-      .pipe(plugins.concat(filesnames.css))
+      .pipe(plugins.concat(filenames.css))
     .pipe(plugins.sourcemaps.write('./'))
     .pipe(gulp.dest(src.min))
 });
@@ -107,16 +116,6 @@ gulp.task('basesass', function(){
     .pipe(gulp.dest(src.min))
 })
 
-// // CSS minifying task
-// gulp.task('cssmin', function () {
-//   var files = config.css.lib;
-//   files.push(src.modules + '/*/css/*.css');
-//   return gulp.src(files)
-//     .pipe(plugins.cssmin())
-//     .pipe(plugins.concat(filesnames.css))
-//     .pipe(gulp.dest(dist.min + '/css'));
-// });
-
 /*
 
   Scripts
@@ -130,6 +129,7 @@ gulp.task('jshint', function () {
     .pipe(plugins.jshint.reporter('default'));
 });
 
+<<<<<<< HEAD
 // JS minifying task
 // gulp.task('uglify', function () {
 //   var files = config.js.lib;
@@ -167,68 +167,33 @@ gulp.task('jshint', function () {
 //     .pipe(gulp.dest(src.base));
 // });
 
+=======
+// concat all scripts into a single js file to be loaded by mustard.js
+>>>>>>> reload config.json when it changes
 gulp.task('scripts:dev', function(){
   return gulp.src(jsSources)
     .pipe(plugins.sourcemaps.init())
-      .pipe(plugins.concat(filesnames.js))
+      .pipe(plugins.concat(filenames.js))
     .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest(src.min));
 })
 
+// same as above but uglify too
 gulp.task('scripts:dist', function(){
   return gulp.src(jsSources)
     .pipe(plugins.sourcemaps.init())
-      .pipe(plugins.concat(filesnames.js))
+      .pipe(plugins.concat(filenames.js))
       .pipe(plugins.uglify({ mangle: false }))
     .pipe(plugins.sourcemaps.write('.'))
     .pipe(gulp.dest(dist.min));
 })
 >>>>>>> mustard cutting working
 
-// // JS minifying task
-// gulp.task('uglify', function () {
-//   var files = config.js.lib;
-//   filesnames.push(src.modules + '/**/*.js');
-//   return gulp.src(files)
-//     .pipe(plugins.ngAnnotate())
-//     .pipe(plugins.uglify({mangle: false}))
-//     .pipe(plugins.concat(filesnames.js))
-//     .pipe(gulp.dest(dist.base + '/js'));
-// });
-
-// // Inject CSS and JS into index.html (for development);
-// gulp.task('inject:dev', function() {
-//   var csslibs = gulp.src(config.css.lib, {read: false});
-//   var csssources = gulp.src([src.modules + '/**/*.css'], {read: false});
-//   var jslibs = gulp.src(config.js.lib, {read: false});
-//   var jssources = gulp.src([src.modules + '/core/app.js', src.modules + '/**/*.js'], {read: false});
-
-//   return gulp.src(src.base + '/index.html')
-//     .pipe(plugins.inject(csslibs, {addRootSlash: false, relative: true, name: 'cssvendors'}))
-//     .pipe(plugins.inject(csssources, {addRootSlash: false, relative: true}))
-//     .pipe(plugins.inject(jslibs, {addRootSlash: false, relative: true, name: 'jsvendors'}))
-//     .pipe(plugins.inject(jssources, {addRootSlash: false, relative: true}))
-
-//     .pipe(gulp.dest(src.base));
-// });
-
-// // Inject CSS and JS into index.html and then minify it and move it to /dist (for production).
-// gulp.task('inject:prod', function() {
-//   var css = gulp.src(dist.base + '/css/' + filesnames.css);
-//   var js = gulp.src(dist.base + '/js/' + filesnames.js);
-
-//   return gulp.src(src.base + '/index.html')
-//     .pipe(gulp.dest(dist.base))
-//     .pipe(plugins.inject(gulp.src(['']), {addRootSlash: false, relative: true, name: 'cssvendors', empty: true, removeTags: true}))
-//     .pipe(plugins.inject(css, {addRootSlash: false, relative: true, removeTags: true}))
-//     .pipe(plugins.inject(gulp.src(['']), {addRootSlash: false, relative: true, name: 'jsvendors', empty: true, removeTags: true}))
-//     .pipe(plugins.inject(js, {addRootSlash: false, relative: true, removeTags: true}))
-//     .pipe(plugins.inject(svgIcons, {
-//       transform: fileContents
-//     }))
-//     .pipe(plugins.minifyHtml({conditionals: true, quotes: true}))
-//     .pipe(gulp.dest(dist.base));
-// });
+// when the config json file changes, reload it
+gulp.task('reloadConfig', function(done){
+  loadConfigFile();
+  runSequence( 'scripts:dev', done );
+})
 
 /*
 
@@ -291,8 +256,17 @@ gulp.task('move', function() {
 });
 
 
-<<<<<<< HEAD
-// Create a new modoule.
+/*
+
+  ###  ###  ######  ######  ##   ## ##    ######
+  ######## ##    ## ##   ## ##   ## ##    ##
+  ## ## ## ##    ## ##   ## ##   ## ##    #####
+  ##    ## ##    ## ##   ## ##   ## ##    ##
+  ##    ##  ######  ######   #####  ##### ######
+
+  Create a new module.
+
+*/
 gulp.task('module', function(done) {
 
   inquirer.prompt([{
@@ -305,7 +279,7 @@ gulp.task('module', function(done) {
       var dirname = answers.module.split('.').pop();
 
       var string = [
-        "(function(AppConfig) {\n",
+        ";(function(AppConfig) {\n",
         "  'use strict';\n\n",
         "  AppConfig.registerModule('" + answers.module + "');\n\n",
         "})(window.AppConfig);"
@@ -327,9 +301,6 @@ gulp.task('module', function(done) {
 
 });
 
-// The watch task.
-gulp.task('watch', function() {
-=======
 
 /*
 
@@ -339,11 +310,12 @@ gulp.task('watch', function() {
     ##  ##   ##     ## ## ##      ##
     ##  ##   ## #####  ##  ## #####
 
+  intended for command line usage
+
 */
 
 // The default (watch) task.
 gulp.task('default', [ 'sass', 'basesass', 'scripts:dev', 'jshint' ], function() {
->>>>>>> mustard cutting working
   plugins.livereload.listen();
 
   gulp.watch([src.modules + '/**/*.sass'], ['sass']);
@@ -353,7 +325,9 @@ gulp.task('default', [ 'sass', 'basesass', 'scripts:dev', 'jshint' ], function()
   gulp.watch([src.base + '/**/*.html'], plugins.livereload.changed);
 
   gulp.watch(src.modules + '/**/*.js', ['jshint', 'scripts:dev']);
-  gulp.watch([src.base + '/*.js', src.min + '*.js'], plugins.livereload.changed);
+  gulp.watch([src.base + '/*.js', src.min + '/*.js'], plugins.livereload.changed);
+
+  gulp.watch('./config.json', ['reloadConfig']);
 });
 
 // The build task.
