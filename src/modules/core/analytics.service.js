58a98d1f6d30df0759258515a909bb1b04a1aa2b
iainmcampbell <hey@iainmc.ca>
2015-11-18 16:36:24 -0500

    fixes

--- src/modules/core/analytics.service.js
+++ src/modules/core/analytics.service.js
@@ -13,12 +13,20 @@
 
   function GoogleAnalytics($q, $http, dataStore, AudioAPI, BackgroundAPI){
 
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    (function(i,s,o,g,r,a,m){
+      i['GoogleAnalyticsObject'] = r;
+      i[r] = i[r]||function(){
+        (i[r].q = i[r].q||[]).push(arguments)
+      };
+      i[r].l = 1*new Date();
+      a = s.createElement(o);
+      m = s.getElementsByTagName(o)[0];
+      a.async = 1;
+      a.src = g;
+      m.parentNode.insertBefore(a,m);
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-',  {name:'a'});
+    window.ga('create', 'UA-',  {name:'a'});
     // optionally add more tracking codes by specifying names for each...
 
     return {
@@ -26,7 +34,7 @@
     }
 
     function pageview(){
-      ga('a.send', 'pageview');
+      window.ga('a.send', 'pageview');
       // ...and referencing their names when you make function calls
     }
 
